3813144489e3fe16992e66d125a96c7bae05e7df
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-10-01 12:11:34 +0530

    [sudhanshu] Add: Added function to print welcome message

---
+++ EmployeeWage.py
@@ -0,0 +1,28 @@
+'''
+
+    @Author: Neelesh Rawat
+    @Date: 12-03-2022
+    @Last Modified by: Neelesh Rawat
+    @Last Modified time: 12-03-11
+    @Title : Write a Python program to get a list, sorted in increasing order by the last
+             element in each tuple from a given list of non-empty tuples.
+
+'''
+
+def welcome():
+    """
+    Description: 
+        Function to display welcome message
+    Parameters:
+        None
+    Returns:
+        None
+    """
+
+    print("Welcome to Employee Wage Computation")
+
+def main():
+    welcome()
+
+if __name__=="__main__":
+    main()
