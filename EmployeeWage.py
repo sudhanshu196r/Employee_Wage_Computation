59c1fdcb50f2776f9af2826249cc16bcc5872a10
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-10-01 15:51:51 +0530

    [sudhanshu] Add: added function to calculate monthly wage of Employee

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -35,7 +35,6 @@ def attendance():
         Present or Absent
     """
     check = random.randint(0,2)
-    print(check)
     return check
 
 
@@ -43,13 +42,24 @@ def dailyWage(wage_per_hour, work_hour):
     """Description: 
         Function to calculate daily wage of Employee
     Parameters:
-        None
+        wage_per_hour: takes the rate per hour for work
+        work_hour: Total work hour
     Returns:
         Daily Wage
     """
     return wage_per_hour*work_hour
 
-
+def monthlyWage(wage_per_hour, work_hour, working_days):
+    """Description: 
+        Function to calculate monthly wage of Employee
+    Parameters:
+        wage_per_hour: takes the rate per hour for work
+        work_hour: Total work hour
+        working_days: total working days for employee
+    Returns:
+        Monthly Wage
+    """
+    return wage_per_hour*work_hour*working_days
 
 
 def main():
@@ -57,15 +67,17 @@ def main():
     wage_per_hour = 20
     fulltime_work = 8
     part_time_work = 4
-    if(attendance==0):
+    working_days = 20
+    if(attendance()==0):
         print("Absent")
-    elif attendance==1:
+    elif attendance() ==1:
         print("Present")
         print(f"Daily wage of Full Time Employee is {dailyWage(wage_per_hour,fulltime_work)}")
+        print(f"Monthly wage of Full Time Employee is {monthlyWage(wage_per_hour,fulltime_work,working_days)}")
     else:
         print("Present")
         print(f"Daily wage of Part Time Employee is {dailyWage(wage_per_hour,part_time_work)}")
-
+        print(f"Monthly wage of Part Time Employee is {monthlyWage(wage_per_hour,part_time_work,working_days)}")
     
 
 if __name__=="__main__":
