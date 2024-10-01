4c6ba68b013703cd86b419778a0e7b5338085187
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-10-01 12:25:57 +0530

    [sudhanshu] Add: added function to calculate daily wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -44,11 +44,24 @@ def attendance():
     return ans
 
 
+def dailyWage():
+    """Description: 
+        Function to calculate daily wage of Employee
+    Parameters:
+        None
+    Returns:
+        Daily Wage
+    """
+    wage_per_hour = 20
+    work_hour = 8
+    return 20*8
+
 
 
 def main():
     welcome()
     print(attendance())
+    print(f"Daily wage of Employee is {dailyWage()}")
 
 if __name__=="__main__":
     main()
