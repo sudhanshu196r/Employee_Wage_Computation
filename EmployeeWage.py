fff0ba2dcb3548f6fb1d16de681b6bef03717465
Sudhanshu kumar <sudhanshu196r@gmail.com>
2024-10-04 10:53:38 +0530

    [sudhanshu] Modify: Modified description

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,11 +1,9 @@
 '''
-
-    @Author: Neelesh Rawat
-    @Date: 12-03-2022
-    @Last Modified by: Neelesh Rawat
-    @Last Modified time: 12-03-11
-    @Title : Write a Python program to get a list, sorted in increasing order by the last
-             element in each tuple from a given list of non-empty tuples.
+    @Author: Sudhanshu Kumar
+    @Date: 01-10-2024
+    @Last Modified by: Sudhanshu Kumar
+    @Last Modified time: 04-10-2024
+    @Title : Python program to print the wage of the employee if employee is present or absent
 
 '''
 
